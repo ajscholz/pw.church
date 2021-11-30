238606f12cbef4cea54d276329f5d7b540c2a82c
ajscholz <yoitsscholzy@gmail.com>
2021-11-30 12:44:17 -0500

    add pw green to color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 const defaultTheme = require("tailwindcss/defaultTheme")
+const colors = require("tailwindcss/colors")
 
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      ...colors,
+      green: {
+        50: "#fafcf6",
+        100: "#f4faed",
+        200: "#e4f2d2",
+        300: "#d4ebb7",
+        400: "#b4db82",
+        500: "#94cc4c",
+        600: "#85b844",
+        700: "#6f9939",
+        800: "#597a2e",
+        900: "#496425",
+      },
+    },
     extend: {
       fontFamily: {
         sans: ["Source Sans Pro", ...defaultTheme.fontFamily.sans],
